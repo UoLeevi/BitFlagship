bf7aee0384d660a2e8957db6ea15ff119bf307b9
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-01-07 16:18:14 +0200

    Demo commit

--- Controllers/RootController.cs
+++ Controllers/RootController.cs
@@ -8,6 +8,6 @@ namespace BitFlagship.Controllers
     {
         [HttpGet("{endPoint?}")]
         public IActionResult GetEcho(string endPoint)
-            => Ok(Json(endPoint ?? "Hello from BitFlagship!"));
+            => Ok($@"""Response"": ""{endPoint ?? "Hello, Li Yan!"}""");
     }
 }
